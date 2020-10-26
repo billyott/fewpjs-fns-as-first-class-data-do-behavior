5fc80ea41afe71285c4d92bfeeb1772adb2e41fa
billyott <willrichott@gmail.com>
2020-10-25 20:47:17 -0400

    Done

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const intTime = parseInt(time, 10)
+  if (intTime < 12 ) {
+    return "Good Morning"
+  } else if (intTime >= 12 && intTime <= 17){
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
